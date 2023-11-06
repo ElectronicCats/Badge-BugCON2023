eb4d6c7470ecfa47f8df0527bd53d6db02508573
deimos <deimoshall@gmail.com>
2023-11-06 13:16:50 -0600

    test: custom uart pins rp2040

--- firmware/Badge/Badge.ino
+++ firmware/Badge/Badge.ino
@@ -22,23 +22,26 @@
 #include <Adafruit_SSD1306.h>
 #include <SPI.h>
 #include <Wire.h>
+#include <ezButton.h>
 
 #include "hardware.h"
 
 #define SCREEN_WIDTH 128  // OLED display width, in pixels
-#define SCREEN_HEIGHT 64  // OLED display height, in pixels 
+#define SCREEN_HEIGHT 32  // OLED display height, in pixels
 
+#define BUTTON_LEFT 27  // GPIO27
+// #define BUTTON_RIGHT 25  // GPIO25
+#define BUTTON_RIGHT 26  // GPIO26
+#define DEBOUNCE_TIME_MS 50
+ 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 // The pins for I2C are defined by the Wire-library.
-// On an arduino UNO:       A4(SDA), A5(SCL)
-// On an arduino MEGA 2560: 20(SDA), 21(SCL)
-// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
 #define OLED_RESET -1        // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C  ///< See datasheet for Address; 0x3D for 128x64, 0x3C ,for 128x32
 
 #if defined(ESP32_DEVKIT) || defined(RP2040)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-#else // ESP32_S3
+#else  // ESP32_S3
 Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #endif
 
@@ -64,8 +67,20 @@ static const unsigned char PROGMEM logo_bmp[] =
      0b01110000, 0b01110000,
      0b00000000, 0b00110000};
 
+ezButton buttonLeft(BUTTON_LEFT);
+ezButton buttonRight(BUTTON_RIGHT);
+
+#ifdef RP2040
+UART Serial2(8, 9, NC, NC);  // TX, RX, RTS, CTS
+#endif
+
 void setup() {
   Serial.begin(9600);
+#ifdef RP2040
+  Serial2.begin(9600);
+#endif
+  buttonLeft.setDebounceTime(DEBOUNCE_TIME_MS);
+  buttonRight.setDebounceTime(DEBOUNCE_TIME_MS);
 
   // Wait for serial monitor to open
   while (!Serial)
@@ -75,10 +90,14 @@ void setup() {
     delay(200);
   }
 
-Serial.println("Board name: " + String(BOARD_NAME));
+  Serial.println("Board name: " + String(BOARD_NAME));
+
+#if defined(RP2040)
+  Serial.println("SERIAL1_TX: " + String(SERIAL1_TX));
+  Serial.println("SERIAL1_RX: " + String(SERIAL1_RX));
+#endif
 
 #if defined(ESP32_DEVKIT) || defined(RP2040)
-  Serial.println("SSD1306 allocation");
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
@@ -88,7 +107,6 @@ Serial.println("Board name: " + String(BOARD_NAME));
 #endif
 
 #if defined(ESP32_S3)
-  Serial.println("SH110X allocation");
   if (!display.begin(SCREEN_ADDRESS)) {
     Serial.println(F("SH110X allocation failed"));
     for (;;)
@@ -100,320 +118,56 @@ Serial.println("Board name: " + String(BOARD_NAME));
   // the library initializes this with an Adafruit splash screen.
   display.display();
   delay(2000);  // Pause for 2 seconds
-
-  // Clear the buffer
-  display.clearDisplay();
-
-  // Draw a single pixel in white
-  display.drawPixel(10, 10, SSD1306_WHITE);
-
-  // Show the display buffer on the screen. You MUST call display() after
-  // drawing commands to make them visible on screen!
-  display.display();
-  delay(2000);
-  // display.display() is NOT necessary after every single drawing command,
-  // unless that's what you want...rather, you can batch up a bunch of
-  // drawing operations and then update the screen all at once by calling
-  // display.display(). These examples demonstrate both approaches...
-
-  testdrawline();  // Draw many lines
-
-  testdrawrect();  // Draw rectangles (outlines)
-
-  testfillrect();  // Draw rectangles (filled)
-
-  testdrawcircle();  // Draw circles (outlines)
-
-  testfillcircle();  // Draw circles (filled)
-
-  testdrawroundrect();  // Draw rounded rectangles (outlines)
-
-  testfillroundrect();  // Draw rounded rectangles (filled)
-
-  testdrawtriangle();  // Draw triangles (outlines)
-
-  testfilltriangle();  // Draw triangles (filled)
-
-  testdrawchar();  // Draw characters of the default font
-
-  testdrawstyles();  // Draw 'stylized' characters
-
-  testdrawbitmap();  // Draw a small bitmap image
-
-  // Invert and restore display, pausing in-between
-  display.invertDisplay(true);
-  delay(1000);
-  display.invertDisplay(false);
-  delay(1000);
-
-  testanimate(logo_bmp, LOGO_WIDTH, LOGO_HEIGHT);  // Animate bitmaps
 }
 
 void loop() {
+  buttonLeft.loop();
+  buttonRight.loop();
+
+  if (buttonLeft.isPressed()) {
+    Serial.println("Left button pressed");
+  }
+
+  if (buttonRight.isPressed()) {
+    Serial.println("Right button pressed");
+  }
+
+  serialTest();
 }
 
-void testdrawline() {
-  int16_t i;
-
-  display.clearDisplay();  // Clear display buffer
-
-  for (i = 0; i < display.width(); i += 4) {
-    display.drawLine(0, 0, i, display.height() - 1, SSD1306_WHITE);
-    display.display();  // Update screen with each newly-drawn line
-    delay(1);
-  }
-  for (i = 0; i < display.height(); i += 4) {
-    display.drawLine(0, 0, display.width() - 1, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-
-  for (i = 0; i < display.width(); i += 4) {
-    display.drawLine(0, display.height() - 1, i, 0, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  for (i = display.height() - 1; i >= 0; i -= 4) {
-    display.drawLine(0, display.height() - 1, display.width() - 1, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-
-  for (i = display.width() - 1; i >= 0; i -= 4) {
-    display.drawLine(display.width() - 1, display.height() - 1, i, 0, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  for (i = display.height() - 1; i >= 0; i -= 4) {
-    display.drawLine(display.width() - 1, display.height() - 1, 0, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-
-  for (i = 0; i < display.height(); i += 4) {
-    display.drawLine(display.width() - 1, 0, 0, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  for (i = 0; i < display.width(); i += 4) {
-    display.drawLine(display.width() - 1, 0, i, display.height() - 1, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);  // Pause for 2 seconds
+void serialTest() {
+  serialSend();
+  serialRead();
 }
 
-void testdrawrect(void) {
-  display.clearDisplay();
+void serialSend() {
+  static unsigned long lastTime = millis();
+  static int counter = 0;
 
-  for (int16_t i = 0; i < display.height() / 2; i += 2) {
-    display.drawRect(i, i, display.width() - 2 * i, display.height() - 2 * i, SSD1306_WHITE);
-    display.display();  // Update screen with each newly-drawn rectangle
-    delay(1);
+  if (millis() - lastTime > 1000) {
+    Serial.println("Hello " + String(counter++));
+#ifdef RP2040
+    Serial2.println("Hello " + String(counter));
+#endif
+    lastTime = millis();
   }
-
-  delay(2000);
 }
 
-void testfillrect(void) {
-  display.clearDisplay();
+void serialRead() {
+#ifdef RP2040
+  if (Serial2.available()) {
+    String input = Serial2.readStringUntil('\n');
+#else
+  if (Serial.available()) {
+    String input = Serial.readStringUntil('\n');
+#endif
 
-  for (int16_t i = 0; i < display.height() / 2; i += 3) {
-    // The INVERSE color is used so rectangles alternate white/black
-    display.fillRect(i, i, display.width() - i * 2, display.height() - i * 2, SSD1306_INVERSE);
-    display.display();  // Update screen with each newly-drawn rectangle
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testdrawcircle(void) {
-  display.clearDisplay();
-
-  for (int16_t i = 0; i < max(display.width(), display.height()) / 2; i += 2) {
-    display.drawCircle(display.width() / 2, display.height() / 2, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testfillcircle(void) {
-  display.clearDisplay();
-
-  for (int16_t i = max(display.width(), display.height()) / 2; i > 0; i -= 3) {
-    // The INVERSE color is used so circles alternate white/black
-    display.fillCircle(display.width() / 2, display.height() / 2, i, SSD1306_INVERSE);
-    display.display();  // Update screen with each newly-drawn circle
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testdrawroundrect(void) {
-  display.clearDisplay();
-
-  for (int16_t i = 0; i < display.height() / 2 - 2; i += 2) {
-    display.drawRoundRect(i, i, display.width() - 2 * i, display.height() - 2 * i,
-                          display.height() / 4, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testfillroundrect(void) {
-  display.clearDisplay();
-
-  for (int16_t i = 0; i < display.height() / 2 - 2; i += 2) {
-    // The INVERSE color is used so round-rects alternate white/black
-    display.fillRoundRect(i, i, display.width() - 2 * i, display.height() - 2 * i,
-                          display.height() / 4, SSD1306_INVERSE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testdrawtriangle(void) {
-  display.clearDisplay();
-
-  for (int16_t i = 0; i < max(display.width(), display.height()) / 2; i += 5) {
-    display.drawTriangle(
-        display.width() / 2, display.height() / 2 - i,
-        display.width() / 2 - i, display.height() / 2 + i,
-        display.width() / 2 + i, display.height() / 2 + i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testfilltriangle(void) {
-  display.clearDisplay();
-
-  for (int16_t i = max(display.width(), display.height()) / 2; i > 0; i -= 5) {
-    // The INVERSE color is used so triangles alternate white/black
-    display.fillTriangle(
-        display.width() / 2, display.height() / 2 - i,
-        display.width() / 2 - i, display.height() / 2 + i,
-        display.width() / 2 + i, display.height() / 2 + i, SSD1306_INVERSE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testdrawchar(void) {
-  display.clearDisplay();
-
-  display.setTextSize(1);               // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);  // Draw white text
-  display.setCursor(0, 0);              // Start at top-left corner
-  display.cp437(true);                  // Use full 256 char 'Code Page 437' font
-
-  // Not all the characters will fit on the display. This is normal.
-  // Library will draw what it can and the rest will be clipped.
-  for (int16_t i = 0; i < 256; i++) {
-    if (i == '\n')
-      display.write(' ');
-    else
-      display.write(i);
-  }
-
-  display.display();
-  delay(2000);
-}
-
-void testdrawstyles(void) {
-  display.clearDisplay();
-
-  display.setTextSize(1);               // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);  // Draw white text
-  display.setCursor(0, 0);              // Start at top-left corner
-  display.println(F("Hello, world!"));
-
-  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);  // Draw 'inverse' text
-  display.println(3.141592);
-
-  display.setTextSize(2);  // Draw 2X-scale text
-  display.setTextColor(SSD1306_WHITE);
-  display.print(F("0x"));
-  display.println(0xDEADBEEF, HEX);
-
-  display.display();
-  delay(2000);
-}
-
-void testdrawbitmap(void) {
-  display.clearDisplay();
-
-  display.drawBitmap(
-      (display.width() - LOGO_WIDTH) / 2,
-      (display.height() - LOGO_HEIGHT) / 2,
-      logo_bmp, LOGO_WIDTH, LOGO_HEIGHT, 1);
-  display.display();
-  delay(1000);
-}
-
-#define XPOS 0  // Indexes into the 'icons' array in function below
-#define YPOS 1
-#define DELTAY 2
-
-void testanimate(const uint8_t *bitmap, uint8_t w, uint8_t h) {
-  int8_t f, icons[NUMFLAKES][3];
-
-  // Initialize 'snowflake' positions
-  for (f = 0; f < NUMFLAKES; f++) {
-    icons[f][XPOS] = random(1 - LOGO_WIDTH, display.width());
-    icons[f][YPOS] = -LOGO_HEIGHT;
-    icons[f][DELTAY] = random(1, 6);
-    Serial.print(F("x: "));
-    Serial.print(icons[f][XPOS], DEC);
-    Serial.print(F(" y: "));
-    Serial.print(icons[f][YPOS], DEC);
-    Serial.print(F(" dy: "));
-    Serial.println(icons[f][DELTAY], DEC);
-  }
-
-  for (;;) {                 // Loop forever...
-    display.clearDisplay();  // Clear the display buffer
-
-    // Draw each snowflake:
-    for (f = 0; f < NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, SSD1306_WHITE);
+    if (input.length() == 0) {
+      Serial.println("Empty input");
+      return;
     }
 
-    display.display();  // Show the display buffer on the screen
-    delay(200);         // Pause for 1/10 second
-
-    // Then update coordinates of each flake...
-    for (f = 0; f < NUMFLAKES; f++) {
-      icons[f][YPOS] += icons[f][DELTAY];
-      // If snowflake is off the bottom of the screen...
-      if (icons[f][YPOS] >= display.height()) {
-        // Reinitialize to a random position, just off the top
-        icons[f][XPOS] = random(1 - LOGO_WIDTH, display.width());
-        icons[f][YPOS] = -LOGO_HEIGHT;
-        icons[f][DELTAY] = random(1, 6);
-      }
-    }
+    Serial.println("Received: " + input);
+    delay(1000);
   }
 }
