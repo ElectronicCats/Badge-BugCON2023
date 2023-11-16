e26cecda7bfe921ce8a7af36bf38d5cf98752b49
deimos <deimoshall@gmail.com>
2023-11-16 13:39:57 -0600

    fix: broken talk words

--- firmware/Badge/UartCommunication.cpp
+++ firmware/Badge/UartCommunication.cpp
@@ -1,42 +1,64 @@
 #include "UartCommunication.h"
 
 const char *validTalks[] = {
-    "Cloud Security - The Blue Team Way",                                                               // 0
-    "Hacking Azure",                                                                                    // 1
-    "DevSecOps",                                                                                        // 2
-    "101 con Frida, para hacking mobile",                                                               // 3
-    "OSINT for Offensive Security. Top 10 herramientas mas populares",                                  // 4
-    "100 vulnerabilidades en 30 dias: Hackeando la mayor compania de seguros del mundo",                // 5
-    "Necesitas un equipo de respuesta ante ciberataques y no sabes por donde comenzar?",                // 6
-    "Tales From a Cloud CSIRT- Let’s deep dive into a Kubernetes (k8s) Infection",                      // 7
-    "Como probar nuestro plan de respuesta a incidentes de manera eficaz?",                             // 8
-    "Brute Forcing and Destroy with IA",                                                                // 9
-    "Evolving the Threat Landscape: The Rise of Post-exploitation",                                     // 10
-    "Querida, descentralice a los ninos",                                                               // 11
-    "Mauro Parra",                                                                                      // 12
-    "El Lado Oscuro de la Concientizacion: Como el Phishing Interno Puede Salvar a tu Empresa",         // 13
-    "Rate limit es importante?",                                                                        // 14
-    "Hacking Azure",                                                                                    // 15
-    "Como meterse a la fila de forma elegante",                                                         // 16
-    "NLPs y NERs: Hablando y compartiendo datos en modelos de Machine Learning en un lenguaje seguro",  // 17
-    "Bug bounty 101",                                                                                   // 18
-    "Security or Espionage",                                                                            // 19
-    "Advanced Persistent Threat - 1: Como desestabilizar un pais",                                      // 20
-    "Definiendo requisitos de seguridad sin morir en el intento",                                       // 21
-    "Mi camino a los 2500 puntos de reputacion en Hackerone",                                           // 22
-    "pulsioximetro IoT usando Arduino y Node MCU",                                                      // 23
-    "Luis Velazquez",                                                                                   // 24
-    "Me fui por bug bounty mobile y esto me paso",                                                      // 25
-    "Porque todos vulneran pero nadie hace contencion y mitigacion",                                    // 26
-    "Hacking etico potenciado por inteligencia artificial: El matrimonio perfecto",                     // 27
-    "Charla 29",
-    "Charla 30",
-    "Charla 31",
-    "Charla 32",
-    "Charla 33",
-    "Charla 34",
-    "Charla 35",
-};
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Cloud Security -  The Blue Team Way",                                                                                                                              // 0
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Hacking Azure",                                                                                                                                                    // 1
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "DevSecOps",                                                                                                                                                        // 2
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "101 con Frida,    para hacking mobile",                                                                                                                            // 3
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "OSINT for         Offensive Security.  Top 10 herramientas  mas populares -      Evelyn Hernandez",                                                                // 4
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "100               vulnerabilidades en  30 dias: Hackeando   la mayor compania de seguros del mundo -  Alan Levy / Damian",                                         // 5
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Necesitas un      equipo de respuesta  ante ciberataques y  no sabes por donde   comenzar? - Marco    Mendoza / Gabriela",                                         // 6
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Tales From a Cloud CSIRT- Lets deep    dive into a          Kubernetes (k8s)     Infection            - Santiago Abastante",                                       // 7
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Como probar       nuestro plan de      respuesta a          incidentes de manera eficaz? - Julio César Muñoz Hernández",                                           // 8
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Brute Forcing and Destroy with IA      - Debug Sec",                                                                                                               // 9
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Evolving the      Threat Landscape:    The Rise of     Post-exploitation         - Maria Isabel       Manjarrez",                                                  // 10
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Querida,          descentralice a los  ninos                - St4t3",                                                                                              // 11
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Revelando el      talon de Aquiles:    explorando trucos de ingenieria social    usando aplicaciones  con interaccion      humana directa       - Mauro Parra",    // 12
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "El Lado Oscuro de la Concientizacion:  Como el Phishing     Interno Puede Salvar a tu Empresa - Jesus Barajas",                                                    // 13
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Rate limit es     importante? -  Felix Morales",                                                                                                                   // 14
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Hacking Azure -   Joaquin Molina",                                                                                                                                  // 15
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Como meterse a la fila de forma        elegante - Virgilio  Castro",                                                                                               // 16
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "NLPs y NERs:      Hablando y           compartiendo datos en modelos de Machine  Learning en un       lenguaje seguro -    Salvador Mendoza",                      // 17
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Bug bounty 101    - Adrian Pedrazzoli",                                                                                                                            // 18
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Security or       Espionage - Dhave    Security",                                                                                                                  // 19
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Advanced          Persistent Threat -  1: Como              desestabilizar un    pais - Jonathan      Palacios",                                                   // 20
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Definiendo        requisitos de        seguridad sin morir  en el intento        - Arturo Sustaita",                                                               // 21
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Mi camino a los   2500 puntos de       reputacion en        Hackerone            - Adrian Pedrazzoli",                                                             // 22
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "pulsioximetro IoT usando Arduino y     Node MCU             - Wulfrano Moreno",                                                                                    // 23
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "De BugCON a       Almoloya: full       disclosure           - Luis Velazquez",                                                                                     // 24
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Me fui por bug    bounty mobile y esto me paso              - Jacobo Tibaquira",                                                                                   // 25
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Porque todos      vulneran pero nadie  hace contencion y    mitigacion           - Irving Paz",                                                                    // 26
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Hacking etico     potenciado por       inteligencia         artificial: El       matrimonio perfecto  - Raul Aguilera      Andrade / Luis Daniel Aguirre Cueva",   // 27
+    /*------col0------+--------col1--------+--------col2--------+--------col3--------+--------col4--------+--------col5--------+--------col6--------+--------col7--------+*/
+    "Cómo hacer        ciberseguridad       Aplicando            Inteligencia         Artificial           - Pablo Corona"};                                            // 28
 
 UartCommunication::UartCommunication() {
   this->communication = false;
@@ -82,6 +104,7 @@ void UartCommunication::updateTalkList(String talkName) {
 
 void UartCommunication::setTalk(uint8_t index) {
   this->talkName = String(validTalks[index]);
+  debug.println("Setting talk: " + this->talkName);
 }
 
 String UartCommunication::getID() {
